d05f874818d818a72c869bd0e87c2085d2305601
yuri0622 <gimpo.s230064@ggh.goe.go.kr>
2024-11-25 14:04:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,6 @@ e = st.selectbox('', list(demographics_dict.keys()))
 demographics = demographics_dict[e]
 
 if st.button('사용시간 예측'): 
-        input_data = [[gender,b,a,platform,demographics]]
+        input_data = [[a,gender,b,platform,demographics]]
         p = model.predict(input_data)
         st.write('인공지능의 예측 사용 시간은', p)
