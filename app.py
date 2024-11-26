c8f8d2fac030193ce190d16798a47745a5460ed3
yuri0622 <gimpo.s230064@ggh.goe.go.kr>
2024-11-26 13:46:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,12 +42,12 @@ with col2:
 
 # 4. 모델 활용
 st.subheader('모델 활용')
-st.write('**** 소득과 나이를 입력하고, 성별을 선택해주세요! ')
+st.write('**** 아래의 정보를 입력해주세요! ')
 
 a = st.number_input(' 나이 입력 ', value=0) 
 b = st.number_input('소득 입력(단위: 달러)',value=0)
 
-gender_dict = {'male':0, 'female':1, 'non-binary':2}
+gender_dict = {'남성':0, '여성':1, '기타':2}
 c = st.selectbox('성별 선택', list(gender_dict.keys()))
 gender = gender_dict[c]
 
@@ -55,11 +55,11 @@ platform_dict = {'instagram':0, 'facebook':1, 'youtube':2}
 d = st.selectbox('주요 사용 SNS 선택', list(platform_dict.keys()))
 platform = platform_dict[d]
 
-demographics_dict = {'urban':0, 'suburban':1, 'rural':2}
-e = st.selectbox('', list(demographics_dict.keys()))
+demographics_dict = {'도심':0, '부도심':1, '시골':2}
+e = st.selectbox('거주지 특성', list(demographics_dict.keys()))
 demographics = demographics_dict[e]
 
-if st.checkbox('?'):
+if st.checkbox('저희 기업은 개인정보 보호법 제17조제1항, 제28조의8제1항에서 지정한 범위에 한하여 정보를 제공합니다. 당신이 우리 기업의 정보를 이용한다면 개인정보 보호법 제18조의 제한을 받습니다. 이에 대해 동의하십니까?'):
     go = 1
 else:
     go = 0
@@ -72,8 +72,6 @@ if st.button('사용시간 예측'):
         st.write('인공지능의 예측 사용 시간은 약', p)
     else:
         st.error('당신은 개인정보를 이용에 대해 책임질 준비가 되어있지 않군요! 당신에게는 정보를 제한합니다.')
-
-
 
 
 # HTML과 CSS를 사용하여 배경색을 핑크색으로 변경
